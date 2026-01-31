5c5a5364d6983dcbfa39a859a171be1b49f35d17
Daniel Sendzik <daniel@sendzik.eu>
2026-01-31 23:48:45 +0100

    Fix config class

--- yume-spring/src/main/kotlin/eu/sendzik/yume/configuration/HomeLocationConfiguration.kt
+++ yume-spring/src/main/kotlin/eu/sendzik/yume/configuration/HomeLocationConfiguration.kt
@@ -3,7 +3,7 @@ package eu.sendzik.yume.configuration
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "yume.location.home")
-class HomeLocationConfiguration {
-    val latitude: Double = 0.0
-    val longitude: Double = 0.0
-}
+class HomeLocationConfiguration(
+    val latitude: Double,
+    val longitude: Double
+)
